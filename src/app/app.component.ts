1157d34b038fc8d84bf064e7d70195a3cb822789
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-11-21 12:32:50 -0400

    🎉 init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -23,17 +22,14 @@ export class AppComponent {
   ];
 
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
   ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    SplashScreen.hide()
+    .catch(error => {
+      console.log(error);
+    });
+    StatusBar.hide()
+    .catch(error => {
+      console.log(error);
     });
   }
 }
